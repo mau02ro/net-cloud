9e15ed71e7c27b607d15e921c48f290e4a6a2727
Mauro Hernández <mauroDeveloper@hotmail.com>
2020-12-07 02:12:30 -0400

    ok

--- main.js
+++ main.js
@@ -3,7 +3,6 @@
 /* Objects */
 var btn_OpenClose_menu;
 var hd__wrapper;
-var hd__label_menu;
 var hd__container;
 var hd__search;
 
@@ -11,15 +10,9 @@ window.addEventListener("load", () => {
   btn_OpenClose_menu = document.getElementById("hd__btnClose");
   hd__wrapper = document.getElementById("hd__wrapper");
   hd__container = document.getElementById("hd__container").firstElementChild;
-  hd__label_menu = document.getElementsByClassName("hd__label-menu");
   hd__search = document.getElementById("hd__search");
 
-  btn_OpenClose_menu.addEventListener("click", hd_show_Header);
-
-  Array.prototype.forEach.call(hd__label_menu, (element) => {
-    console.log(element)
-    element.addEventListener("click", (event) => hd_open_Menu(event));
-  })
+  btn_OpenClose_menu.addEventListener("click", hd_show_Header); 
 })
 
 function validationWidth(value = 1025){
@@ -48,7 +41,7 @@ function hd_show_Header() {
       let width;
 
       if(validationWidth()){
-        width = (width__app*93)/100;
+        width = (width__app*75)/100;
       }else{
         width = width__app;
       }
@@ -79,34 +72,19 @@ function hd_show_Header() {
 /* Open Menu */
 /* ---------------- */
 /* ---------------- */
-function hd_open_Menu(event){
-  event.preventDefault();
+$( ".hd__label-menu" ).click(function(event) {
+  if(!validationWidth(1800)){
+    event.preventDefault();
 
-  if(true){
-    let target = event.target;
-    let menu = event.target.parentNode.nextElementSibling;
-
-    console.log(target, menu)
-    if(!menu.classList.contains("hd_OpenMenu")){
-      target.classList.add("hd_OpenMenu-label");
-      menu.classList.add("hd_OpenMenu");
-
-      menu.style.height  = "auto";
-
-      let height = `${menu.clientHeight}px`;
-
-      menu.style.height  = "0px";
-
-      setTimeout(() => {
-        menu.style.height  = height ;
-      }, 0)   
-    }else{
-      menu.style.height = "0px";
-      target.classList.remove("hd_OpenMenu-label");
-
-      menu.addEventListener("transitionend", () => {
-        menu.classList.remove("hd_OpenMenu");
-      }, {once: true})
-    }
+    $(this).toggleClass("hd__label-menu-active");
+    $(this).siblings("ul").slideToggle("slow");
   }
-}
+});
+$( ".hd__label-subMenu" ).click(function(event) {
+  if(!validationWidth(1800)){
+    event.preventDefault();
+
+    $(this).toggleClass("hd__label-subMenu-active");
+    $(this).siblings("ul").slideToggle("slow");
+  }
+});
